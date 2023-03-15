acce5ab51346eaab526c492d5a39cc6df8393ac9
Devoid_Bark <petros_9002@mail.ru>
2023-03-15 17:49:53 +0300

    Change in graph_view.dart

--- lib/graph/graph_view.dart
+++ lib/graph/graph_view.dart
@@ -42,6 +42,11 @@ class OpenPainter extends CustomPainter {
       ..strokeCap = StrokeCap.round //rounded points
       ..strokeWidth = 30;
 
+    var paintForPoint = Paint()
+      ..color = const Color(0xff63aa65)
+      ..strokeCap = StrokeCap.round //rounded points
+      ..strokeWidth = 25;
+
     var paint2 = Paint()
       ..color = const Color(0xffef0037)//rounded points
       ..strokeWidth = 2;
@@ -56,50 +61,11 @@ class OpenPainter extends CustomPainter {
 
     final List<Offset> points = [];
     canvas.drawCircle(Offset(size.width / 2, size.height / 2), 140, paint3);
-    final dlina = matrix.length; // 3  2   4
-    final segment = 360 / dlina; //120 180 90
 
-    for(var i =0; i < dlina;i++){
-      // if(matrix.length == 4){
-      //   if(i == 0){
-      //     points.add(const Offset(75, 75));
-      //   }
-      //   if(i == 1){
-      //     points.add(const Offset(225, 75));
-      //   }
-      //   if(i == 2){
-      //     points.add(const Offset(75, 225));
-      //   }
-      //   if(i == 3){
-      //     points.add(const Offset(225, 225));
-      //   }
-      // }
-      // if(matrix.length == 5){
-      //   if(i == 0){
-      //     points.add(const Offset(150, 50));
-      //   }
-      //   if(i == 1){
-      //     points.add(const Offset(50, 150));
-      //   }
-      //   if(i == 2){
-      //     points.add(const Offset(250, 150));
-      //   }
-      //   if(i == 3){
-      //     points.add(const Offset(100, 250));
-      //   }
-      //   if(i == 4){
-      //     points.add(const Offset(200, 250));
-      //   }
-      // }
-      // if(matrix.length == 2){
-      //   if(i == 0){
-      //     points.add(const Offset(75, 75));
-      //   }
-      //   if(i == 1){
-      //     points.add(const Offset(225, 75));
-      //   }
-      // }
-      points.add(Offset((150 + 140 * cos(segment + segment * i)),(150 + 140 * sin(segment + segment * i))));
+    final segment = 360 / matrix.length;
+    for(var i =0; i < matrix.length;i++){
+      final angle = 2 * pi * (i / matrix.length) + segment;
+      points.add(Offset((cos(angle) * 140 + (size.width / 2)), (sin(angle) * 140 + (size.width / 2))));
     }
 
 
@@ -137,15 +103,26 @@ class OpenPainter extends CustomPainter {
               matrix[j][i] = 0;
           }
         }
-        canvas.drawPoints(PointMode.points, points, paint1);
-        for(var i =0;i < points.length;i++){
-          TextSpan span = TextSpan(style: const TextStyle(color: Colors.black,fontWeight: FontWeight.bold), text: "${i + 1}");
-          TextPainter tp = TextPainter(text: span, textAlign: TextAlign.left, textDirection: TextDirection.ltr);
-          tp.layout();
-          tp.paint(canvas, Offset(points[i].dx -5.0, points[i].dy - 8.0));
-        }
       }
     }
+
+
+    // рисование вершин
+    if(points.length > 6){
+      canvas.drawPoints(PointMode.points, points, paintForPoint);
+    }
+    else{
+      canvas.drawPoints(PointMode.points, points, paint1);
+    }
+
+    // рисование индекса вершины
+    for(var i =0;i < points.length;i++){
+      TextSpan span = TextSpan(style: const TextStyle(color: Colors.black,fontWeight: FontWeight.bold), text: "${i + 1}");
+      TextPainter tp = TextPainter(text: span, textAlign: TextAlign.left, textDirection: TextDirection.ltr);
+      tp.layout();
+      tp.paint(canvas, Offset(points[i].dx -5.0, points[i].dy - 8.0));
+    }
+
   }
 
   @override
